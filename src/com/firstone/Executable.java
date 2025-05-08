2613d6b64bcb55e4dabbe9e6e7977b2ace3140d7
vinodh1988 <k.c.vinodh@hotmail.com>
2025-05-08 08:22:35 +0530

    firstone

---
+++ src/com/firstone/Executable.java
@@ -0,0 +1,12 @@
+package com.firstone;
+
+public class Executable {
+   public static void main(String[] args) {
+	 System.out.println("First Program");
+	 Person p = new Person();
+	 p.display();
+	 
+	 // p is reference variable;
+	 //new Person() -- object creation
+  }
+}
---
+++ src/com/firstone/Person.java
@@ -0,0 +1,14 @@
+package com.firstone;
+
+public class Person {
+   int sno;
+   String name;
+   String city; // properties or instance variables
+   
+   void display() {
+	   System.out.println("Sno:"+sno);
+	   System.out.println("Name:"+name);
+	   System.out.println("City:"+city);
+	   
+   }   //methods or instance methods
+}
