b63a08a9c112e4bc0f41a68122ff115f024cb7c0
vinodh1988 <k.c.vinodh@hotmail.com>
2025-05-09 07:41:07 +0530

    latest

--- src/com/firstone/Executable.java
+++ src/com/firstone/Executable.java
@@ -3,9 +3,20 @@ package com.firstone;
 public class Executable {
    public static void main(String[] args) {
 	 System.out.println("First Program");
-	 Person p = new Person();
+	 Person p = new Person();//default constructor will not create if you have any other form of constructor
+	 Person p1= new Person(2,"Rohan","Chennai");
+	 Person p2= new Person(3,"James","Mumbai");
 	 p.display();
-	 
+	 p1.display();
+	 p2.display();
+	// p.sno=1;p.name="Aadhi"; //wrong practice
+	 p.setSno(1);
+	 p.setName("Aadhi");
+	 System.out.println("---------------------------");
+	 p.display();
+	 p1.display();
+	 p2.display();
+	
 	 // p is reference variable;
 	 //new Person() -- object creation
   }
--- src/com/firstone/Person.java
+++ src/com/firstone/Person.java
@@ -1,14 +1,49 @@
 package com.firstone;
 
 public class Person {
-   int sno;
-   String name;
-   String city; // properties or instance variables
+  private int sno; //instance variable
+  private String name;
+  private String city; // properties or instance variables
+  public Person(){
+	   
+   }
    
-   void display() {
+  public  Person(int sno ,String name,String city) { //sno,name,city are local variables
+	   this.sno=sno;
+	   this.name=name;
+	   this.city=city;
+   }
+   public void display() {
 	   System.out.println("Sno:"+sno);
 	   System.out.println("Name:"+name);
 	   System.out.println("City:"+city);
 	   
    }   //methods or instance methods or member methods/member functions
+
+public int getSno() {
+	return sno;
+}
+
+public void setSno(int sno) {
+
+	this.sno = sno;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getCity() {
+	return city;
+}
+
+public void setCity(String city) {
+	this.city = city;
+}
+   
+   
 }
